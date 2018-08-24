afa954052a9a5a47fef61232642cb322ad28ee1e
¨abhskg¨ <abhasgorain@gmail.com>
2018-08-24 23:45:11 +0530

    ¨HorizontalRecyclerCreatedXML¨

--- app/src/main/java/com/example/oem/mntc_app/Recycler/HorizontalRecycler.java
+++ app/src/main/java/com/example/oem/mntc_app/Recycler/HorizontalRecycler.java
@@ -4,6 +4,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
-public class HorizontalRecycler {
+import com.example.oem.mntc_app.R;
 
+public class HorizontalAdapter extends RecyclerView.Adapter<HorizontalAdapter.HorizontalViewHolder>{
+
+    public class HorizontalViewHolder extends RecyclerView.ViewHolder {
+
+        // Code for The first horizontal Scroller in the main activity
+
+    }
 }
